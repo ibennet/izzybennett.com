f5ed4128c231cfd7fa53b7c5fefb1c88b2e25fbd
Izzy Bennett <ibennet@gmu.edu>
2020-05-25 00:01:41 -0400

    Updates

---
+++ precache-manifest.257e4ab7d846abf5fd05a3809570603a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d51a98b21f61cb83869937aae84be09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1422cedcd30f9085f71",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "df56d9e71aca0669db52",
+    "url": "/static/css/main.f116edd5.chunk.css"
+  },
+  {
+    "revision": "e1422cedcd30f9085f71",
+    "url": "/static/js/2.0c31ab66.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.0c31ab66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df56d9e71aca0669db52",
+    "url": "/static/js/main.221a9713.chunk.js"
+  },
+  {
+    "revision": "5baf99d392acf4e8a17f",
+    "url": "/static/js/runtime-main.648e12ef.js"
+  }
+]);
